5b2de9ff29ab20bfc02306d000e1e14438689286
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-16 22:06:18 +0600

    создание детального представления задачи

---
+++ source/todo_app/views/todo_detail_view.py
@@ -0,0 +1,11 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render, get_object_or_404
+
+from todo_app.models import TODO
+
+
+def detail_view(request: WSGIRequest, pk):
+    todo = get_object_or_404(TODO, pk=pk)
+    return render(request, 'article.html', context={
+        'todo': todo
+    })
