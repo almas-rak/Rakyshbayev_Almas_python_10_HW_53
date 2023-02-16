ef6a6997e3ebed173700c1d11dde32d8f709419f
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-16 20:55:18 +0600

    регистрация в админке модели TODO

--- source/todo_app/admin.py
+++ source/todo_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from todo_app.models import TODO
+
+
 # Register your models here.
+
+class TODOAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'status', 'date_of_completion')
+    list_filter = ('title', 'status', 'date_of_completion')
+    search_fields = ('id', 'title', 'description', 'status', 'date_of_completion')
+    list_editable = ('title', 'description', 'status', 'date_of_completion')
+
+
+admin.site.register(TODO, TODOAdmin)
