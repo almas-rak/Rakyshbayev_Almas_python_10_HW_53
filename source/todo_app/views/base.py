5f324d45c88344f9dd062122c1c2bfad3dcd18a6
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-16 21:50:12 +0600

    исправлена ошибка контекста todo -> todos

--- source/todo_app/views/base.py
+++ source/todo_app/views/base.py
@@ -7,7 +7,7 @@ from todo_app.models import TODO
 def index_view(request: WSGIRequest):
     todos = TODO.objects.all()
     context = {
-        'todo': todos
+        'todos': todos
     }
 
     return render(request, 'index.html', context=context)
