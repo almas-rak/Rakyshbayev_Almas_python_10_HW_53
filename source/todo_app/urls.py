5962b3c7ea04e4c273e4dd05b2b940f00864fdcc
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-16 22:09:21 +0600

    добавлена ссылка детального представления задачи

--- source/todo_app/urls.py
+++ source/todo_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from todo_app.views.base import index_view
+from todo_app.views.todo_detail_view import detail_view
 
 urlpatterns = [
-    path('', index_view, name='home')
+    path('', index_view, name='home'),
+    path('todo/<int:pk>', detail_view, name='todo_detail')
 ]
