5beddfea04a4de5cc86f80e1f83eca3074df258d
Almas Rakyshbaev <kasper10.0711@gmail.com>
2023-02-17 18:51:44 +0600

    добавил ссылку на редактирование

--- source/todo_app/urls.py
+++ source/todo_app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from todo_app.views.base import index_view
 from todo_app.views.todo_detail_view import detail_view
+from todo_app.views.todo_edit import todo_edit
 
 urlpatterns = [
     path('', index_view, name='home'),
-    path('todo/<int:pk>', detail_view, name='todo_detail')
+    path('todo/<int:pk>', detail_view, name='todo_detail'),
+    path('todo/edit/<int:pk>', todo_edit, name='edit_todo'),
 ]
